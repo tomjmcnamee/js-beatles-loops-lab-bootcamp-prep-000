05fe9bbab9bd996a742192a8d65eb698e97470e9
Tom McNamee <tom@tomjmcnamee.com>
2019-03-05 02:50:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
 // add solution here
-function theBeatlesPlay(mus, inst) {
-  var arr1 = []
-  for ( var counter = 0; counter === mus.length ; counter++) { 
-    arr1.push(`${mus[counter]} plays ${inst[counter]}`)
-}
-   return arr1
-}
+
 
